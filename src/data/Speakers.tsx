5e7fc1cc025a0b170de4b47669824270adfc26f2
Katarzyna Sikora <stawiamy.na.wapw@gmail.com>
2025-06-24 16:41:01 +0200

    Add speakers photos and english descriptions

--- src/data/Speakers.tsx
+++ src/data/Speakers.tsx
@@ -7,45 +7,51 @@ export type Speaker = {
 
 export const speakers: Speaker[] = [
     {
-        image: "/static/cat0.jpg",
-        name: "Puszek",
+        image: "/static/About/Speakers/TOM_AVERMAETE.png",
+        name: "Tom Avermaete",
+        description_pl: "Trochę dłuższy opis tego pięknego kotka",
+        description_en: "Full Professor for the History and Theory of Urban Design and Vice-Dean of the Department of Architecture (D-ARCH) at ETH Zürich, Switzerland",
+    },
+    {
+        image: "/static/About/Speakers/ROBERTO_CAVALLO.png",
+        name: "Roberto Cavallo",
+        description_pl: "Trochę dłuższy opis tego pięknego kotka",
+        description_en: "Associate professor in the Department of Architecture at the Faculty of Architecture & the Built Environment, Delft University of Technology",
+    },
+    {
+        image: "/static/About/Speakers/KEES_CHRISTIAANSE.png",
+        name: "Kees Christiaanse",
         description_pl: "Trochę dłuższy opis tego pięknego kotka",
         description_en: "A description of this beautiful kitten",
     },
     {
-        image: "/static/cat1.jpg",
-        name: "Puszek",
+        image: "/static/About/Speakers/MICHAEL_J_CROSBIE.png",
+        name: "Michael J. Crosbie",
+        description_pl: "Trochę dłuższy opis tego pięknego kotka",
+        description_en: "Professor of Architecture at the University of Hartford",
+    },
+    {
+        image: "/static/About/Speakers/JUANJO_GALAN.png",
+        name: "Juanjo Galan",
+        description_pl: "Trochę dłuższy opis tego pięknego kotka",
+        description_en: "Associate Professor in the Department of Urban Planning of the Universitat Politècnica de València (Spain",
+    },
+    {
+        image: "/static/About/Speakers/MAŁGORZATA MADER.png",
+        name: "Małgorzata Mader",
+        description_pl: "Trochę dłuższy opis tego pięknego kotka",
+        description_en: "Assistant professor and architect based at Lodz University of Technology",
+    },
+    {
+        image: "/static/About/Speakers/APOLONIA_BEGONA_SERRANO_LANZAROTE.png",
+        name: "Apolonia Begoña Serrano Lanzarote",
         description_pl: "Trochę dłuższy opis tego pięknego kotka",
         description_en: "A description of this beautiful kitten",
     },
     {
-        image: "/static/cat2.jpg",
-        name: "Puszek z bardzo długim imieniem które na pewno nie wybuchnie",
+        image: "/static/About/Speakers/SPELA_VEROVSEK.png",
+        name: "Špela Verovšek",
         description_pl: "Trochę dłuższy opis tego pięknego kotka",
-        description_en: "A description of this beautiful kitten",
+        description_en: "Assistant professor and researcher in the field of urbanism at the Faculty of Architecture, University of Ljubljana",
     },
-    {
-        image: "/static/cat3.jpg",
-        name: "Puszek",
-        description_pl: "Trochę dłuższy opis tego pięknego kotka",
-        description_en: "A description of this beautiful kitten",
-    },
-    {
-        image: "/static/cat4.jpg",
-        name: "Puszek",
-        description_pl: "Trochę dłuższy opis tego pięknego kotka",
-        description_en: "A description of this beautiful kitten",
-    },
-    {
-        image: "/static/cat5.jpg",
-        name: "Puszek",
-        description_pl: "Trochę dłuższy opis tego pięknego kotka",
-        description_en: "A description of this beautiful kitten",
-    },
-    {
-        image: "/static/cat7.jpg",
-        name: "Puszek",
-        description_pl: "Trochę dłuższy opis tego pięknego kotka",
-        description_en: "A description of this beautiful kitten",
-    }
 ];
