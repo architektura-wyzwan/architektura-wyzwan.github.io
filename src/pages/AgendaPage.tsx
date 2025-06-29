869d8c1fee2663fc410c7530ecc25cdac5c4fd9f
Katarzyna Sikora <stawiamy.na.wapw@gmail.com>
2025-06-29 22:37:39 +0200

    Update agenda layout on mobile

--- src/pages/AgendaPage.tsx
+++ src/pages/AgendaPage.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Divider, Grid, Tab, Typography} from "@mui/material";
+import {Divider, Grid, Tab, Typography, useMediaQuery, useTheme} from "@mui/material";
 import {Translation} from "../common/Translation";
 import {agenda} from "../data/Agenda";
 import {TabContext, TabList, TabPanel} from "@mui/lab";
@@ -10,6 +10,8 @@ export default function AgendaPage() {
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
+    const theme = useTheme();
+    const smallOrLarger = useMediaQuery(theme.breakpoints.up('sm'));
     return (
         <PageLayout title_pl="Program" title_en="Agenda" wide={false}>
             <TabContext value={value}>
@@ -30,10 +32,16 @@ export default function AgendaPage() {
                         >
                             {agenda.items.map((item) => (
                                 <>
-                                    <Grid size={2}>
+                                    <Grid size={{
+                                        xs: 3,
+                                        sm: 2,
+                                    }}>
                                         <Typography variant="bold">{item.time}</Typography>
                                     </Grid>
-                                    <Grid size={14}>
+                                    <Grid size={{
+                                        xs: 13,
+                                        sm: 14,
+                                    }}>
                                         <Typography variant="bold">
                                             <Translation pl={item.description_pl} en={item.description_en}/>
                                         </Typography>
@@ -41,19 +49,29 @@ export default function AgendaPage() {
                                     {item.items.map((item) => (
                                         <>
                                             <Grid size={2}/>
-                                            <Grid size={7}>
+                                            <Grid size={{
+                                                xs: 14,
+                                                sm: 7,
+                                            }}>
                                                 <Typography variant="bolder">
                                                     {item.title !== undefined ? item.title :
                                                         <Translation pl={item.title_pl} en={item.title_en}/>}
                                                 </Typography>
                                             </Grid>
-                                            <Grid size={3}>
+                                            {smallOrLarger ? <></> : <Grid size={2}/>}
+                                            <Grid size={{
+                                                xs: 7,
+                                                sm: 3,
+                                            }}>
                                                 <Typography variant="body1">
                                                     {item.speaker !== undefined ? item.speaker :
                                                         <Translation pl={item.speaker_pl} en={item.speaker_en}/>}
                                                 </Typography>
                                             </Grid>
-                                            <Grid size={4}>
+                                            <Grid size={{
+                                                xs: 7,
+                                                sm: 4,
+                                            }}>
                                                 <Typography variant="body1">
                                                     <Translation pl={item.institution_pl} en={item.institution_en}/>
                                                 </Typography>
