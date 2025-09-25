d58b5fac3bf2181949ff0e57f5126941f342ab56
Katarzyna Sikora <stawiamy.na.wapw@gmail.com>
2025-09-25 12:03:30 +0200

    Create gallery 2025

--- src/data/news/opening_pavilion_of_community.tsx
+++ src/data/news/opening_pavilion_of_community.tsx
@@ -6,7 +6,7 @@ export const opening_pavilion_of_community: ArticleContent = {
     title_en: "Opening of the Pavilion of Community",
     subtitle_pl: "Pawilon Wspólnoty w Muzeum Łazienki Królewskie – nowa przestrzeń refleksji i integracji, symbol więzi i symbiozy ",
     subtitle_en: "The Community Pavilion at the Royal Łazienki Museum – a new space dedicated to reflection and integration, serving as a symbol of unity and symbiosis.",
-    image: "/static/About/News/Opening of the Pavilion of Community/686254218bf14.jpg",
+    image: "/static/About/News/Opening of the Pavilion of Community/Foto-151.jpg",
     content: [
 
         paragraph(
@@ -17,7 +17,8 @@ export const opening_pavilion_of_community: ArticleContent = {
             "Pawilon Wspólnoty to więcej niż konstrukcja – to przestrzeń, która w architektonicznej formie stawia pytania o sens wspólnoty i współistnienia ludzi, przestrzeni oraz natury. Inspiracją dla jego formy stał się „cyrk realis”, czyli egalitarna struktura, w której nie istnieje centrum dominacji, a każda część ma taką samą wagę. Kluczowym elementem kompozycji jest dwanaście drewnianych modułów, stanowiących znak jedności oraz trzy centralne donice – symbol równowagi i harmonii.",
             "The Community Pavilion is more than just a structure; it's a space that, in architectural form, poses questions about the meaning of community and the coexistence of people, space, and nature. Its design was inspired by the 'circus realis' — an egalitarian structure with no dominant center, where every part holds equal weight. The composition's key elements are twelve wooden modules, symbolizing unity, and three central planters, representing balance and harmony.",
         ),
-        two_images("/static/About/News/Opening of the Pavilion of Community/686253c252d5e.jpg", "/static/About/News/Opening of the Pavilion of Community/d0040ee6-0aa1-46ad-80d8-1d1ea0b92198.jpg"),
+        two_images("/static/About/News/Opening of the Pavilion of Community/Foto-48.jpg", "/static/About/News/Opening of the Pavilion of Community/Foto-116.jpg"),
+        big_image ("/static/About/News/Opening of the Pavilion of Community/Foto-15.jpg"),
         paragraph(
             "Donice inspirowane twórczością Aliny Scholtz – wybitnej polskiej architektki zieleni – tworzą mikroekosystemy, które nie tylko wzbogacają wizualnie obiekt, ale pełnią też funkcję edukacyjną i symboliczną. Pawilon zaprasza do kontemplacji, oferując miejsce sprzyjające refleksji, dialogowi i bliskości z przyrodą. Jego forma – wznosząca się ku górze – wskazuje na duchowe i naturalne powiązania człowieka z otoczeniem oraz podkreśla tymczasowość architektury i wagę szacunku dla systemów natury.",
             "The planters, inspired by the work of Alina Scholtz – an eminent Polish landscape architect – create micro-ecosystems that not only visually enrich the structure but also serve an educational and symbolic function. The pavilion invites contemplation, offering a space conducive to reflection, dialogue, and closeness with nature. Its upward-rising form points to humanity's spiritual and natural connections with the environment, while also emphasizing the temporary nature of architecture and the importance of respecting natural systems."
@@ -26,7 +27,7 @@ export const opening_pavilion_of_community: ArticleContent = {
             "Usytuowanie pawilonu za historycznym Ermitażem nadaje realizacji szczególnego charakteru. Nie dominuje on nad otoczeniem, lecz z nim współistnieje – wpisując się w pejzaż ogrodów Muzeum Łazienek Królewskich jako pomost między przeszłością a teraźniejszością. Obiekt stanowi przykład, jak współczesna architektura może być świadomym uczestnikiem miejsca, w którym powstaje.",
             "The pavilion's location behind the historic Ermitage lends a special character to the realization. It doesn't dominate its surroundings but rather coexists with them, blending into the landscape of the Royal Łazienki Museum gardens as a bridge between past and present. The structure serves as an example of how contemporary architecture can be a conscious participant in the place where it is created."
         ),
-        big_image ("/static/About/News/Opening of the Pavilion of Community/68623a139656a.jpg"),
+        big_image ("/static/About/News/Opening of the Pavilion of Community/Foto-84.jpg"),
         paragraph(
             "Za projekt i realizację pawilonu odpowiada zespół studentów kierunku A3 – Architektura Technologii i Struktury pod kierunkiem prof. Anny Marii Wierzbickiej oraz kadry dydaktycznej: mgr. inż. arch. Macieja Kaufmana, mgr. inż. arch. Szymona Kalaty i dr inż. arch. krajobrazu Kingi Zinowiec-Cieplik.",
             "The design and construction of the pavilion were overseen by a team of students from the A3 – Architecture of Technology and Structure program, under the guidance of Professor Anna Maria Wierzbicka and the teaching staff: Maciej Kaufman, M.Sc., Eng. Arch.; Szymon Kalata, M.Sc., Eng. Arch.; and Kinga Zinowiec-Cieplik, Ph.D., Eng. Landscape Arch."
@@ -39,5 +40,7 @@ export const opening_pavilion_of_community: ArticleContent = {
             "Pawilon Wspólnoty można odwiedzać do końca listopada 2025 roku. Znajduje się on na terenie Muzeum Łazienki Królewskie, za budynkiem Ermitażu przy ul. Myśliwieckiej 1A.",
             "The Community Pavilion is open to visitors until the end of November 2025. It's located within the Royal Łazienki Museum grounds, behind the Ermitage building at Myśliwiecka 1A."
         ),
+        big_image ("/static/About/News/Opening of the Pavilion of Community/Foto-144.jpg"),
+        two_images("/static/About/News/Opening of the Pavilion of Community/Foto-126.jpg", "/static/About/News/Opening of the Pavilion of Community/Foto-134.jpg"),
     ]
 }
