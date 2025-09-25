692b16e0395a59ee277f97ef93836d89ffae6a40
Katarzyna Sikora <stawiamy.na.wapw@gmail.com>
2025-09-25 12:53:50 +0200

    Update footer
    (cherry picked from commit f70c3bcc81f4f2848ee8db68206469bd63444712)

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -7,12 +7,7 @@ import { Box } from "@mui/material";
 export default function AgendaPage() {
     return (
         <PageLayout title_pl="Kontakt" title_en="Contact" wide={false}>
-            <Box sx={{pl: 4}}>
-                <Translation pl="Profesor Arch. Anna Maria Wierzbicka" en="Professor Arch. Anna Maria Wierzbicka"/>
-                <br/>
-                <Translation pl="arch. Magdalena Duda" en="Arch. Magdalena Duda"/>
-            </Box>
-            <Contact inverted={false}/>
+            <Contact inverted={false} light={false}/>
         </PageLayout>
     );
 }
